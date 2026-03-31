2fc86bd3db44aa4dd707c80030022b849e83f5ef
ajengpembayun <ajengpembayunks@email.com>
2026-03-31 14:42:02 +0700

    Tugas-one

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="bg-blue-500 p-4">
+      <h1 className="text-white text-2xl">React + Tailwind 🚀</h1>
+      <p className="text-white">Nama: Ajeng Pembayun Kartika Sari</p>
+      <p className="text-white">NIM: A11.2023.15127</p>
+    </div>
+  )
+}
+
+
+export default App;
