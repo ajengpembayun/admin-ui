e6146d71a98745cf0536d8f41c1f8b63355175cc
ajengpembayun <ajengpembayunks@gmail.com>
2026-04-07 22:44:32 +0700

    menggunakan utility class tailwindcss

--- App.jsx
+++ App.jsx
@@ -1,12 +1,9 @@
 function App() {
   return (
-    <div className="bg-blue-500 p-4">
-      <h1 className="text-white text-2xl">React + Tailwind 🚀</h1>
-      <p className="text-white">Nama: Ajeng Pembayun Kartika Sari</p>
-      <p className="text-white">NIM: A11.2023.15127</p>
-    </div>
-  )
+    <>
+      <h1>React + Tailwind 🚀</h1>
+    </>
+  );
 }
-
 
 export default App;
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
-function App() {
-  return (
-    <div className="bg-blue-500 p-4">
-      <h1 className="text-white text-2xl">React + Tailwind 🚀</h1>
-      <p className="text-white">Nama: Ajeng Pembayun Kartika Sari</p>
-      <p className="text-white">NIM: A11.2023.15127</p>
-    </div>
-  )
-}
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="min-h-screen bg-neutral-800  flex flex-col items-center justify-center">
+        <div className="flex items-center justify-center space-x-8 mb-12">
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="w-24 h-24" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="w-24 h-24 animate-spin" 
+            alt="React logo"
+            style={{animationDuration: "10s"}}
+             />
+          </a>
+        </div>
+
+        <h1 className="text-white text-6x1 font-bold mb-16">Vite + React</h1>
+
+        <div className="text-sm space-y-6 mb-8">
+          <button onClick={() => setCount((count) => count + 1)}
+            className="bg-neutral-900 text-white py-2 px-6 rounded-lg text-lg">
+            count is {count}
+          </button>
+
+          <p className="text-neutral-400">
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+
+        <p className="text-neutral-500">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </>
+  );
+}
 
 export default App;
