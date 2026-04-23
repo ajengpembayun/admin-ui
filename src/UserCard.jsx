ce476be75f13f632ec171960d3c41d5e1673bfb0
ajengpembayun <ajengpembayunks@gmail.com>
2026-04-23 14:43:02 +0700

    update user card props

--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 function UserCard(props) {
     const {name, email, street, city} = props;
+    const [clicked, setClicked] = useState(false);
+
+      // Fungsi handler
+  function handleClick() {
+    setClicked(true);
+  }
 
     return (
         <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition-shadow">
@@ -10,8 +16,14 @@ function UserCard(props) {
             <span className="font-medium">Email:</span> {email}
           </p>
           <p className="text-gray-600">
-            <span className="font-medium">Address:</span> {street}, {city}
+            <span className="font-medium">Address:</span> 
+            {street}, {city}
           </p>
+          <button className={clicked ? "bg-green-600 text-white p-2 rounded-md" : "bg-gray-600 text-white p-2 rounded-md"}
+          onClick={() => setClicked(true)}
+          >
+            {clicked ? "Tombol sudah diklik" : "Silakan Klik"}
+          </button>
         </div>
       );
 }
